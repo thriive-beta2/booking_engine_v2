33a3b1d428d0e5d4bb938068d49b1b137cb01277
prathamesh animator <88764232+prathamthriive@users.noreply.github.com>
2026-03-22 01:58:53 +0530

    Update dataService.ts

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -30,7 +30,7 @@ export interface EventResponse {
 }
 
 export const fetchData = async <T>(path: string): Promise<T> => {
-  const response = await fetch(`http://localhost:3000/data/${path}`);
+  const response = await fetch(`https://booking-engine.thriive.in/data/${path}`);
   if (!response.ok) throw new Error(`Failed to fetch ${path}`);
   return response.json();
 };
